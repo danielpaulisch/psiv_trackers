492f95413a4a05da7193bcffd75b801ba24ecfeb
1606338 <1606338@uab.cat>
2023-10-20 16:07:51 +0200

    TRACKER OBJETO NUVOE

--- import numpy as np.py
+++ import numpy as np.py
@@ -4,6 +4,11 @@ import numpy as np
 import tracker1
 
 
-tr = tracker1.Tracker()
-print(tr._ids)
+M = np.zeros((4,3))
 
+for i in range(4):
+    for J in range(3):
+        M[i,J] = J
+
+print(np.where(M == np.min(M))[0],np.where(M == np.min(M))[1] )
+print(M)
