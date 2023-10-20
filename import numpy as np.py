68d11bb414750f32b5ff4dc16d9304ec2c619620
1606338 <1606338@uab.cat>
2023-10-20 14:13:50 +0200

    TRACKER 1.3

--- import numpy as np.py
+++ import numpy as np.py
@@ -1,16 +1,9 @@
 import numpy as np
 
 
-m = np.zeros((2,3))
+import tracker1
 
 
-for i in range(2):
-    for j in range(3):
-        m[i , j] = i + j
-    
-
-p = np.where(m == np.max(m) )
-print(m.sum())
-print(m)
-
+tr = tracker1.Tracker()
+print(tr._ids)
 
